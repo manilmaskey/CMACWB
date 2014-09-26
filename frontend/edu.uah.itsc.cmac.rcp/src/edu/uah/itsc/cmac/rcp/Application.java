9fa86d73938f96bf2b75c08abe65a6297581f0e9
Shreedhan Shrestha <shreedhan.s@gmail.com>
2014-09-26 16:10:26 -0500

    Working version to allow user change workspace. User needs to specify workspace in the beginning of the program

--- frontend/edu.uah.itsc.cmac.rcp/src/edu/uah/itsc/cmac/rcp/Application.java
+++ frontend/edu.uah.itsc.cmac.rcp/src/edu/uah/itsc/cmac/rcp/Application.java
@@ -1,7 +1,17 @@
 package edu.uah.itsc.cmac.rcp;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
+import org.eclipse.jface.dialogs.IInputValidator;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.window.Window;
+import org.eclipse.osgi.service.datalocation.Location;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
@@ -11,23 +21,64 @@ import org.eclipse.ui.PlatformUI;
  */
 public class Application implements IApplication {
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.equinox.app.IApplication#start(org.eclipse.equinox.app.IApplicationContext)
 	 */
 	public Object start(IApplicationContext context) {
 		Display display = PlatformUI.createDisplay();
+
+		try {
+			setWorkspace();
+		}
+		catch (Exception e) {
+			MessageDialog.openError(Display.getDefault().getActiveShell(), "Error", "Cannot open the workspace");
+			System.exit(0);
+			e.printStackTrace();
+		}
+
 		try {
 			int returnCode = PlatformUI.createAndRunWorkbench(display, new ApplicationWorkbenchAdvisor());
 			if (returnCode == PlatformUI.RETURN_RESTART) {
 				return IApplication.EXIT_RESTART;
 			}
 			return IApplication.EXIT_OK;
-		} finally {
+		}
+		finally {
 			display.dispose();
 		}
 	}
 
-	/* (non-Javadoc)
+	private void setWorkspace() throws IOException, MalformedURLException {
+		String workspaceLoc = System.getProperty("user.home") + System.getProperty("file.separator") + "cwb.workspace";
+
+		InputDialog dialog = new InputDialog(Display.getDefault().getActiveShell(), "Set workspace",
+			"Select workspace", workspaceLoc, new IInputValidator() {
+
+				@Override
+				public String isValid(String newText) {
+					if (newText.isEmpty())
+						return "You must select your workspace";
+					return null;
+				}
+			});
+
+		if (dialog.open() == Window.OK) {
+			workspaceLoc = dialog.getValue();
+		}
+		else
+			System.exit(0);
+
+		Location instanceLocation = Platform.getInstanceLocation();
+		if (instanceLocation.isSet())
+			instanceLocation.release();
+		instanceLocation.set(new URL("file", null, workspaceLoc), false);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.equinox.app.IApplication#stop()
 	 */
 	public void stop() {
