5a452a6fa0d69eabf0d3a69a566fddbbe0b77be5
Shreedhan Shrestha <shreedhan.s@gmail.com>
2014-10-02 15:24:55 -0500

    Modified Application.java to include 'Browse' button to be able to choose folder for workspace

--- frontend/edu.uah.itsc.cmac.rcp/src/edu/uah/itsc/cmac/rcp/Application.java
+++ frontend/edu.uah.itsc.cmac.rcp/src/edu/uah/itsc/cmac/rcp/Application.java
@@ -7,12 +7,21 @@ import java.net.URL;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
-import org.eclipse.jface.dialogs.IInputValidator;
-import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.window.Window;
 import org.eclipse.osgi.service.datalocation.Location;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Monitor;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
 
@@ -28,7 +37,6 @@ public class Application implements IApplication {
 	 */
 	public Object start(IApplicationContext context) {
 		Display display = PlatformUI.createDisplay();
-
 		try {
 			setWorkspace();
 		}
@@ -52,28 +60,101 @@ public class Application implements IApplication {
 
 	private void setWorkspace() throws IOException, MalformedURLException {
 		String workspaceLoc = System.getProperty("user.home") + System.getProperty("file.separator") + "cwb.workspace";
+		Display display = Display.getDefault();
+		final Shell shell = new Shell(display, SWT.SHELL_TRIM & (~SWT.RESIZE) & (~SWT.MAX) & (~SWT.MIN) & (~SWT.CLOSE));
 
-		InputDialog dialog = new InputDialog(Display.getDefault().getActiveShell(), "Set workspace",
-			"Select workspace", workspaceLoc, new IInputValidator() {
+		shell.setLayout(new GridLayout(4, false));
+		shell.setText("Select workspace");
+		GridData emptyData = new GridData();
+		emptyData.horizontalSpan = 4;
 
-				@Override
-				public String isValid(String newText) {
-					if (newText.isEmpty())
-						return "You must select your workspace";
-					return null;
+		Label emptyLabel1 = new Label(shell, SWT.NONE);
+		emptyLabel1.setLayoutData(emptyData);
+
+		Label workspaceLabel = new Label(shell, SWT.NONE);
+		workspaceLabel.setText("Workspace: ");
+
+		GridData gridData = new GridData();
+		gridData.horizontalSpan = 2;
+		gridData.widthHint = 300;
+		final Text workspaceLocText = new Text(shell, SWT.BORDER);
+		workspaceLocText.setText(workspaceLoc);
+		workspaceLocText.setLayoutData(gridData);
+
+		GridData buttonData = new GridData();
+		// buttonData.heightHint = 20;
+		buttonData.widthHint = 90;
+
+		Button browseButton = new Button(shell, SWT.PUSH);
+		browseButton.setText("Browse");
+		browseButton.setLayoutData(buttonData);
+
+		Label emptyLabel2 = new Label(shell, SWT.NONE);
+		emptyLabel2.setLayoutData(emptyData);
+
+		Label emptyLabel3 = new Label(shell, SWT.NONE);
+
+		// GridData compositeData = new GridData();
+		// compositeData.horizontalAlignment = SWT.CENTER;
+		//
+		// Composite buttonComposite = new Composite(shell, SWT.NONE);
+		Button okButton = new Button(shell, SWT.PUSH);
+		okButton.setText("Ok");
+		okButton.setLayoutData(buttonData);
+
+		Button cancelButton = new Button(shell, SWT.PUSH);
+		cancelButton.setText("Cancel");
+		cancelButton.setLayoutData(buttonData);
+		Label emptyLabel4 = new Label(shell, SWT.NONE);
+
+		Label emptyLabel5 = new Label(shell, SWT.NONE);
+		emptyLabel2.setLayoutData(emptyData);
+
+		browseButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				DirectoryDialog dialog = new DirectoryDialog(shell);
+				String selectedDir = dialog.open();
+				if (selectedDir != null)
+					workspaceLocText.setText(selectedDir);
+			}
+		});
+
+		okButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent event) {
+				String selectedDir = workspaceLocText.getText();
+				if (selectedDir.isEmpty()) {
+					MessageDialog.openError(shell, "Error", "You must select workspace directory");
+					return;
 				}
-			});
+				Location instanceLocation = Platform.getInstanceLocation();
+				if (instanceLocation.isSet())
+					instanceLocation.release();
+				try {
+					instanceLocation.set(new URL("file", null, selectedDir), false);
+				}
+				catch (Exception e) {
+					MessageDialog.openError(shell, "Error", "Cannot select workspace");
+				}
+				shell.close();
+			}
+		});
 
-		if (dialog.open() == Window.OK) {
-			workspaceLoc = dialog.getValue();
+		cancelButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				System.exit(0);
+			}
+		});
+		shell.pack();
+		setWindowPosition(display, shell);
+		shell.open();
+
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
 		}
-		else
-			System.exit(0);
-
-		Location instanceLocation = Platform.getInstanceLocation();
-		if (instanceLocation.isSet())
-			instanceLocation.release();
-		instanceLocation.set(new URL("file", null, workspaceLoc), false);
 	}
 
 	/*
@@ -92,5 +173,16 @@ public class Application implements IApplication {
 					workbench.close();
 			}
 		});
+	}
+
+	private void setWindowPosition(Display display, Shell shell) {
+		Monitor primary = display.getPrimaryMonitor();
+		Rectangle bounds = primary.getBounds();
+		Rectangle rect = shell.getBounds();
+
+		int x = bounds.x + (bounds.width - rect.width) / 2;
+		int y = bounds.y + (bounds.height - rect.height) / 2;
+
+		shell.setLocation(x, y);
 	}
 }
